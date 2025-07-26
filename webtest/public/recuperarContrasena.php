fe56efe3d1444e6203cd360d64b9a94d191f3c8d
MartinDidoli <ixanol99@gmail.com>
2025-07-26 00:07:59 -0300

    Issue #2 Solucionado, reutilizado el head en distintos archivos

--- webtest/public/recuperarContrasena.php
+++ webtest/public/recuperarContrasena.php
@@ -1,18 +1,6 @@
 <?php
-session_start();
-?>
-<!DOCTYPE html>
-<html lang="es">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Recuperar contraseña</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<?php
+$tituloPagina = 'Rosario Plaza Grande - Recuperar Contraseña';
+require_once("includes/headPublic.php");
 if(isset($_GET["login"])){
     switch ($_GET["login"]){
         case "malaClave":
--- webtest/src/novedadesadmin.php
+++ webtest/src/novedadesadmin.php
@@ -1,18 +1,6 @@
 <?php
-session_start();
-?>
-<!DOCTYPE html>
-<html lang="es">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Novedades (Admin)</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
-    <link rel="stylesheet" href="../public/css/style.css">
-</head>
-<?php
+$tituloPagina = 'Novedades - Admin';
+require_once("../public/includes/headPublic.php");
 include "conectarDB.php";
 $cant_por_pag=5;
 $pagina=isset($_GET["pagina"]) ? $_GET["pagina"]:null;
